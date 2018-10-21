6bc1c121f1c28386025b1ae824908b5171a10eb3
Plus <plus@omsklug.com>
2018-10-21 15:46:20 +0500

    Обновление скрипта
    Обновление

--- kerio-vpn.sh
+++ kerio-vpn.sh
@@ -1,6 +1,14 @@
+#!/bin/bash 
+testzenity=$(sudo apt list | grep zenity-common)
+if [ -n $"testzenity" ]; then
+echo start #	zenity --info --timeout=1 --width=300 --title="Установка zenity" --text="Zenity уже установлен"
+else
+	zenity --info --timeout=1 --width=300 --title ="Установка Zenity" --text="Требуется установка графического интерфейса командной строки"
+sudo apt install zenity
+fi
 tu="Подключение_клиента_KerioVPN"
 
-kes=$(zenity --list --text="What do you think about your PC?" \
+kes=$(zenity --list --text="Подключение kerio VPN" \
     --radiolist --column "Pick" --column "Opinion" --width 350\
     TRUE "Выход" \
     FALSE $tu \
@@ -19,25 +27,30 @@ echo "работает"
 fi
 
 if [ $kes = $tu ]; then
-sudo service kerio-kvc start
+gksu service kerio-kvc start
 echo "Подключение клиента KerioVPN";
 fi
 
 if [ $kes = Отключение_Kerio_VPN_клиент ]; then
-sudo service kerio-kvc stop
+gksu service kerio-kvc stop
 echo "Отключение Kerio VPN клиент";
+fi
+
+if [ $kes = Изминение_настроек_подключения_Kerio_VPN ]; then
+zenity --info --width=350 --height=350 --title ="Изминение настроек Kerio VPN клиента" --text="Для изминение настроек Kerio VPN выполните в терминале: sudo dpkg-reconfigure kerio-control-vpnclient"
+echo "Изминение_настроек_подключения_Kerio_VPN";
 fi
 
 if [ $kes = Установить_Kerio_VPN_клиент  ]; then
 wget -P /tmp/ http://download.kerio.com/eu/dwn/kerio-control-vpnclient-linux.deb
 sleep 1
-sudo dpkg -i --force-architecture --force-depends kerio-control-vpnclient-linux.deb
-sudo dpkg-reconfigure kerio-control-vpnclient
+gksu dpkg -i --force-architecture --force-depends kerio-control-vpnclient-linux.deb
+gksu dpkg-reconfigure kerio-control-vpnclient
 echo "Установить Kerio VPN клиент";
 fi
 
 if [ $kes = Удалить_Пакет_Kerio_VPN_клиент ]; then
-sudo dpkg -r kerio-control-vpnclient;
+gksu dpkg -r kerio-control-vpnclient;
 sleep 1
 echo "Удалить Пакет Kerio VPN клиент";
 fi
